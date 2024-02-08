7257774e7eb8faf80ee7ab10eb3e1eb4374e535b
UnniKrishnaPanicker <unni.krishnan@citrusinformatics.com>
2024-02-08 12:21:28 +0530

    update usecounter

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,5 +1,4 @@
 import { useCallback, useState } from "react";
-import PropTypes from "prop-types";
 import { validateInitialValue } from "../helpers/validateInitialValue";
 
 type IUseCounter = {
@@ -56,14 +55,6 @@ const useCounter = (initialValue: number = 0): IUseCounter => {
     [validatedInitialValue]
   );
   return { count, increment, decrement, reset };
-};
-
-useCounter.PropTypes = {
-  initialValue: PropTypes.number.isRequired,
-};
-
-useCounter.defaultProps = {
-  initialValue: 0,
 };
 
 export default useCounter;
